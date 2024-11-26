5f75fe08ac198b7f958130a916bd751b3f56fb9e
Rafael-Creator-max <rafael.pereztarira@student.kuleuven.be>
2024-11-26 11:45:21 +0100

    minor changes

--- index.php
+++ index.php
@@ -10,8 +10,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        * {
+             box-sizing: border-box;
+            }
+        
+        h1{
+            text-align: center;
+           
+        }
+
+        section{
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                gap: 10rem;
+                align-items: center;
+                background-color
+            }
+
+    </style>
 </head>
 <body>
+    <h1>Guess the country !!</h1>
+
+    <section>
+        <form action = "index.php" methode = "post">
+        <label for="photo">Country to guess >(°-°)/:</label> <br> 
+        
     
+            <a> <button type = "button" > Peru </button></a> <br>
+            <a> <button type = "button" > Afghanistan </button></a> <br>
+            <a> <button type = "button" > India </button></a> <br>
+            <a> <button type = "button" > Mongolia </button></a> <br>
+    </section>
+            
+
+        </form>
+        
+
 </body>
 </html>
